bcf245f03a25f9f633d178c75ab1ecd138551301
Snowbird <jakounter@yahoo.com>
2018-09-24 20:43:59 -0500

    a

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -9,7 +9,7 @@ const Navbar = props => (
       <ul className="navbar-nav col-md-12">
         <li className="col-md-4">
           <Link
-            to="clickygame/"
+            to="https://snowbird1986.github.io/clickygame/"
             className={
               window.location.pathname === "/"
                 ? "nav-item active"
